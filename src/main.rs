ca5151955364b0009d3e83fa2c0e9613b64e2c55
Connor Hanley <connordhanley@gmail.com>
2022-06-15 12:09:05 -0500

    Added some image rendering

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::render::WindowCanvas; // type WindowCanvas = Canvas<Window>
+use sdl2::render::{WindowCanvas, Texture}; // type WindowCanvas = Canvas<Window>
+use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
 /// This is our traditional render function for our game loop:
 /// it first sets to a drawing color (of color specified), clears the canvas
 /// of any previous items, and then finally `present()`'s the rendering to the wider
 /// game loop to let it know that the action is a-okay and complete.
-fn render(canvas: &mut WindowCanvas, color: Color) {
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+
+    canvas.copy(&texture, None, None)?;
+
     canvas.present();
+
+    Ok(())
 }
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
 
     let window = video_subsystem
         .window("game tutorial", 800, 600)
@@ -28,6 +35,9 @@ pub fn main() -> Result<(), String> {
         .into_canvas()
         .build()
         .expect("could not initialize the canvas");
+        
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -50,7 +60,7 @@ pub fn main() -> Result<(), String> {
         
         // Render
         let color = Color::RGB(i, 64, 255 - i);
-        render(&mut canvas, color);
+        render(&mut canvas, color, &texture)?;
 
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
